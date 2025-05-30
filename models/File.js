30f077ea62cab3b86bf0f89acfdd6a6b82bb906c
JalpNP <154866803+JalpNP@users.noreply.github.com>
2025-05-30 12:26:36 +0530

    File.js uploaded

---
+++ models/File.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const fileSchema = new mongoose.Schema({
+  name: String,
+  mobile: Number,
+  dob: Date,
+  email: String,
+  permanentadd: String,
+  presentadd: String,
+  pincode: String,
+  institutename: String,
+  education: String,
+  currentstatus: String,
+  techopted: String,
+  duration: String,
+  fees: Number,
+  pendingfees: Number,
+  referedby: String,
+  photo: String,
+  aadhar: String,
+
+  // Fields for offer letter
+  internshipDate: String,
+  startDate: String,
+  responsibilities: String,
+  returnBy: String,
+  offerStatus: {
+    type: String,
+    enum: ["offered", "pending"],
+    default: "pending",
+  },
+
+  // Fields for review status
+  description: {
+    type: String,
+    default: "",
+  },
+  status: {
+    type: String,
+    enum: ["reviewed", "unreviewed"],
+    default: "unreviewed",
+  },
+
+  // New fields for completion letter
+  completionDate: String,
+  completionTo: String,
+  completionName: String,
+  completionStartDate: String,
+  completionEndDate: String,
+  workSummary: String,
+  performance: String,
+  appreciation: String,
+  completionStatus: {
+    type: String,
+    enum: ["sent", "pending"],
+    default: "pending",
+  },
+}, { timestamps: true });
+
+const File = mongoose.model('File', fileSchema);
+
+export default File;
